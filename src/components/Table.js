c0359dd9f2f1a284390d2bd77aa67f3c115b7e04
Bruno Marques <brunorcmarques@gmail.com>
2022-09-13 16:17:09 -0300

    Requisito 6 - Adicionado conteúdo da tabela pelo estado global

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div>
         <table>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            { expenses
+              .map(({ id, value, description, currency, method, tag, exchangeRates }) => (
+                <tr key={ id }>
+                  <td>{description}</td>
+                  <td>{tag}</td>
+                  <td>{method}</td>
+                  <td>{Number(value).toFixed(2)}</td>
+                  <td>{exchangeRates[currency].name}</td>
+                  <td>{Number(exchangeRates[currency].ask).toFixed(2)}</td>
+                  <td>{(value * exchangeRates[currency].ask).toFixed(2)}</td>
+                  <td>Real</td>
+                </tr>
+              ))}
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  ...state.wallet,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape(PropTypes.any)).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
